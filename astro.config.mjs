ec3cbd627998318904739759ca975211ed229b3f
httpparam <parammehta06@icloud.com>
2025-12-10 18:07:31 +0530

    Update Astro configuration to set site URL and modify Dockerfile to change application port from 3000 to 4321

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://httpparam.me/',
   server: {
     host: true, // allow external hosts (0.0.0.0)
   },
