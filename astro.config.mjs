44a00bb803a335dd1858c60b22d571349a288ab7
httpparam <parammehta06@icloud.com>
2025-12-10 18:11:19 +0530

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://httpparam.me/',
-  server: {
-    host: true, // allow external hosts (0.0.0.0)
-  },
   vite: {
     preview: {
       host: true,
-      allowedHosts: ['httpparam.me', 'www.httpparam.me'],
-    },
-    server: {
-      host: true,
+      allowedHosts: ['httpparam.me'],
     },
   },
 });
