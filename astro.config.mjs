caf343adc72f5646d0811953ac4b4413fff53b03
x1-xh <parammehta06@icloud.com>
2026-07-13 01:37:50 +0530

    config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://httpparam.me/',
+  site: 'https://x1xh.xyz',
   vite: {
     define: {
       'import.meta.env.COMMIT_HASH': JSON.stringify(process.env.COMMIT_HASH || 'unknown'),
