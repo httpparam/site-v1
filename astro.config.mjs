a4fa66e8112fd5b13739a4b5950ac7ac8f5b69ed
x1-xh <parammehta06@icloud.com>
2026-09-05 18:50:10 +0530

    Allow all hosts in preview server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://x1xh.xyz',
+  preview: { allowedHosts: true },
   vite: {
     define: {
       'import.meta.env.COMMIT_HASH': JSON.stringify(process.env.COMMIT_HASH || 'unknown'),
